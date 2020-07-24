385ca80240f0911aa6c84a677b922ea9118492fe
phultquist <phultquist@imsa.edu>
2020-07-24 14:13:03 -0500

    removed ugly code

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ let header, classes = [];
 let labels = ['4.0', '3.67', '3.33', '3.0', '2.67', '2.33', '2.0', '1.67', '1.0'];
 let labelText = ['A (4.0)', 'A- (3.67)', 'B+ (3.33)', 'B (3.0)', 'B- (2.67)', 'C+ (2.33)', 'C (2.0)', 'C- (1.67)', 'D (1.0)'];
 
-// read('all').then(() => {
-
-// })
-
 app.get('/', (req, res) => {
 	res.set('Cache-Control', 'public, max-age=25200');
 
